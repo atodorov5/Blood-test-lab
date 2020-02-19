de48f4919bbda21dbb56e3d46ceb6d5c1a7288d3
atodorov5 <atodorov98@gmail.com>
2020-02-19 21:36:00 +0200

    Login design

---
+++ BloodTestLab/BloodTestLab/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BloodTestLab
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (username.Text == "nasko" && password.Password.ToString() == "123")
+            {
+                UserWindow userW = new UserWindow();
+                userW.Show();
+                this.Close();
+            }
+            else
+                loginError.Content = "Wrong username or password!";
+        }
+    }
+}
