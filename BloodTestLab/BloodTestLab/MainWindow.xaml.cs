9d2e639163def93bf26117733deb8d50d5d7ea5a
atodorov5 <atodorov98@gmail.com>
2020-02-23 14:42:17 +0200

    added User and Admin Windows

--- BloodTestLab/BloodTestLab/MainWindow.xaml.cs
+++ BloodTestLab/BloodTestLab/MainWindow.xaml.cs
@@ -27,14 +27,41 @@ namespace BloodTestLab
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            if (username.Text == "nasko" && password.Password.ToString() == "123")
+            String salt = CreateSalt(10);
+            String hashedpass = GenerateSHA256Hash(password.Password.ToString(), "top");
+            
+            if (username.Text == "nasko" && GenerateSHA256Hash(password.Password.ToString(), "top") == hashedpass)
             {
                 UserWindow userW = new UserWindow();
                 userW.Show();
                 this.Close();
             }
+            else if(username.Text=="admin" && password.Password.ToString() == "admin")
+            {
+                AdminWindow adminW = new AdminWindow();
+                adminW.Show();
+                this.Close();
+            }
             else
                 loginError.Content = "Wrong username or password!";
         }
+
+        public String CreateSalt(int size)
+        {
+            var rng = new System.Security.Cryptography.RNGCryptoServiceProvider();
+            var buff = new byte[size];
+            rng.GetBytes(buff);
+            return Convert.ToBase64String(buff);
+        }
+
+        public String GenerateSHA256Hash(String input,String salt)
+        {
+            byte[] bytes = System.Text.Encoding.UTF8.GetBytes(input + salt);
+            System.Security.Cryptography.SHA256Managed sha256hashsring = new System.Security.Cryptography.SHA256Managed();
+            byte[] hash = sha256hashsring.ComputeHash(bytes);
+            return Convert.ToBase64String(hash);
+        }
+
+    
     }
 }
